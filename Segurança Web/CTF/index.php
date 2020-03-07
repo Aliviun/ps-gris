b5c97bc8269e293e619670d37fe8418a584d4228
Aliviun <53957365+Aliviun@users.noreply.github.com>
2020-03-07 13:53:16 -0300

    back-end

--- Segurança Web/CTF/index.php
+++ Segurança Web/CTF/index.php
@@ -1,3 +1,7 @@
+/*Esse é a parte back-end do website que se comunica com o server e contra a visualização
+dos arquivos HTML. A vulnerabilidade consiste em não haver verificação de autorização para acessar
+os arquivos, sendo assim a aplicação é vulnerável a Path Traversal Injection*/
+
 <?php
 
 $page = $_GET['page'] ?? 'home';
